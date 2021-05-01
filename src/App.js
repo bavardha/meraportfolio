de768ecdb60e3085b849f8df5b35e009f40ee1c0
Balanagu Harsha Vardhan <balanaguharshavardhan@gmail.com>
2021-05-01 15:00:37 +0530

    Added particle js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div className="App">
+      <Particles className="background" width="100vw" height="100vh" params={{
+                particles: {
+                  color: {
+                    value: "#ffffff",
+                  },
+                  links: {
+                    color: "#ffffff",
+                    distance: 10,
+                    enable: true,
+                    opacity: 0.3,
+                    width: 1,
+                  },
+                  collisions: {
+                    enable: true,
+                  },
+                  move: {
+                    direction: "none",
+                    enable: true,
+                    outMode: "bounce",
+                    random: false,
+                    speed: 1,
+                    straight: false,
+                  },
+                  number: {
+                    density: {
+                      enable: true,
+                      value_area: 800,
+                    },
+                    value: 80,
+                  },
+                  opacity: {
+                    value: 0.5,
+                  },
+                  shape: {
+                    type: "circle",
+                  },
+                  size: {
+                    random: true,
+                    value: 5,
+                  },
+                },
+            		interactivity: {
+                  detectsOn: "canvas",
+                  events: {
+                    onClick: {
+                      enable: true,
+                      mode: "push",
+                    },
+                    onHover: {
+                      enable: true,
+                      mode: "grab",
+                    },
+                    resize: true,
+                  },
+                  modes: {
+                    bubble: {
+                      distance: 400,
+                      duration: 2,
+                      opacity: 0.8,
+                      size: 40,
+                    },
+                    push: {
+                      quantity: 4,
+                    },
+                    repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                    },
+                  },
+                }
+            	}}/>
       <Header />
       <section id="Intro">
         <header className="App-header">
